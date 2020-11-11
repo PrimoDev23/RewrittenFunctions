377b32e4b86130e5d945c86edba4327c5caba5b4
PrimoDev23 <lexx.ps2711@gmail.com>
2020-11-11 09:16:44 +0100

    Initial commit with some small functions
    * TryParseInt
    * Ends/StartsWith
    
    Signed-off-by: PrimoDev23 <lexx.ps2711@gmail.com>

---
+++ RewrittenFunctions/Math.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RewrittenFunctions
+{
+    /// <summary>
+    /// Adds faster Functions for mathematical Calculations
+    /// </summary>
+    public static class Math
+    {
+        public static bool TryParseToInt(this string s, out Int32 number)
+        {
+            number = 0;
+
+            if (s == null || s.Length == 0)
+            {
+                return false;
+            }
+
+            int negative = s[0] == 45 ? 1 : 0;
+
+            //This is for obvious overflows
+            if (s.Length > (negative == -1 ? 11 : 10))
+            {
+                return false;
+            }
+
+            for (int i = negative; i < s.Length; i++)
+            {
+                char curr = s[i];
+                if (!char.IsDigit(curr))
+                {
+                    number = 0;
+                    return false;
+                }
+
+                number *= 10;
+                number += curr - '0';
+
+                //For not that obvious overflows
+                if (number < 0)
+                {
+                    number = 0;
+                    return false;
+                }
+            }
+
+            if (negative == -1)
+            {
+                number *= -1;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ RewrittenFunctions/Strings.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RewrittenFunctions
+{
+    public static class Strings
+    {
+        //This makes inining possible
+        public static bool EndsWith(this string input, string end)
+        {
+            if (end.Length > input.Length)
+            {
+                throw new ArgumentException("The end-string can't be longer than the string to check");
+            }
+            return EndsWithFull(input, end);
+        }
+
+        private static bool EndsWithFull(string input, string end)
+        {
+            int inputLastIndex = input.Length - 1;
+            int endLastIndex = end.Length - 1;
+            for (int i = 0; i < end.Length; i++)
+            {
+                if (input[inputLastIndex - i] != end[endLastIndex - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool StartsWith(string input, string start)
+        {
+            if (start.Length > input.Length)
+            {
+                throw new ArgumentException("The start-string can't be longer than the string to check");
+            }
+
+            for (int i = 0; i < start.Length; i++)
+            {
+                if (input[i] != start[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
