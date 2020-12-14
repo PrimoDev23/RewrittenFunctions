6a18ba1917143bfe5c2b556623ddb5e5bb115a37
PrimoDev23 <lexx.ps2711@gmail.com>
2020-12-14 09:58:09 +0100

    Add methods to get getter and setter for unknown types
    * Useful when u have a list of objects that are basically a user defined class and u want to access properties
    -> Like in a datagrid

--- RewrittenFunctions/ExpressionTree/GetterSetter.cs
+++ RewrittenFunctions/ExpressionTree/GetterSetter.cs
@@ -15,8 +15,12 @@ namespace RewrittenFunctions.ExpressionTree
     /// <typeparam name="T2">return / set value type</typeparam>
     public static class GetterSetter<T, T2>
     {
+        #region KnownTypes
+
+        #region Getter
+
         /// <summary>
-        /// Get the getter method for a property
+        /// Get the getter method for a property in a known type class
         /// </summary>
         /// <param name="propertyName">Name of the property</param>
         /// <returns>Getter method</returns>
@@ -27,7 +31,7 @@ namespace RewrittenFunctions.ExpressionTree
         }
 
         /// <summary>
-        /// Get the getter method for a field
+        /// Get the getter method for a field in a known type class
         /// </summary>
         /// <param name="fieldName">Name of the field</param>
         /// <returns>Getter method</returns>
@@ -37,8 +41,12 @@ namespace RewrittenFunctions.ExpressionTree
             return Expression.Lambda<Func<T, T2>>(Expression.Field(param, fieldName), param).CompileFast();
         }
 
+        #endregion Getter
+
+        #region Setter
+
         /// <summary>
-        /// Get the setter method for a property
+        /// Get the setter method for a property in a known type class
         /// </summary>
         /// <param name="propertyName">Name of the property</param>
         /// <returns>Setter method</returns>
@@ -54,7 +62,7 @@ namespace RewrittenFunctions.ExpressionTree
         }
 
         /// <summary>
-        /// Get the setter method for a field
+        /// Get the setter method for a field in a known type class
         /// </summary>
         /// <param name="fieldName">Name of the field</param>
         /// <returns>Setter method</returns>
@@ -68,5 +76,79 @@ namespace RewrittenFunctions.ExpressionTree
 
             return Expression.Lambda<Action<T, T2>>(assign, param, newValue).CompileFast();
         }
+
+        #endregion Setter
+
+        #endregion KnownTypes
+
+        #region UnknownTypes
+
+        #region Getter
+
+        /// <summary>
+        /// Get the getter method for a property in an unknown type class
+        /// </summary>
+        /// <param name="propertyName">Name of the property</param>
+        /// <param name="type">Type of the containing class</param>
+        /// <returns>Getter method</returns>
+        public static Func<T, T2> GetGetterProperty(string propertyName, Type type)
+        {
+            ParameterExpression param = Expression.Parameter(type, "param");
+            return Expression.Lambda<Func<T, T2>>(Expression.Property(param, propertyName), param).CompileFast();
+        }
+
+        /// <summary>
+        /// Get the getter method for a field in an unknown type class
+        /// </summary>
+        /// <param name="fieldName">Name of the field</param>
+        /// <param name="type">Type of the containing class</param>
+        /// <returns>Getter method</returns>
+        public static Func<T, T2> GetGetterField(string fieldName, Type type)
+        {
+            ParameterExpression param = Expression.Parameter(type, "param");
+            return Expression.Lambda<Func<T, T2>>(Expression.Field(param, fieldName), param).CompileFast();
+        }
+
+        #endregion Getter
+
+        #region Setter
+
+        /// <summary>
+        /// Get the setter method for a property in an unknown type class
+        /// </summary>
+        /// <param name="propertyName">Name of the property</param>
+        /// <param name="type">Type of the containing class</param>
+        /// <returns>Setter method</returns>
+        public static Action<T, T2> GetSetterProperty(string propertyName, Type type)
+        {
+            ParameterExpression param = Expression.Parameter(type, "param");
+            ParameterExpression newValue = Expression.Parameter(typeof(T2), "newValue");
+
+            MemberExpression property = Expression.Property(param, propertyName);
+            BinaryExpression assign = Expression.Assign(property, newValue);
+
+            return Expression.Lambda<Action<T, T2>>(assign, param, newValue).CompileFast();
+        }
+
+        /// <summary>
+        /// Get the setter method for a field in an unknown type class
+        /// </summary>
+        /// <param name="fieldName">Name of the field</param>
+        /// <param name="type">Type of the containing class</param>
+        /// <returns>Setter method</returns>
+        public static Action<T, T2> GetSetterField(string fieldName, Type type)
+        {
+            ParameterExpression param = Expression.Parameter(type, "param");
+            ParameterExpression newValue = Expression.Parameter(typeof(T2), "newValue");
+
+            MemberExpression field = Expression.Field(param, fieldName);
+            BinaryExpression assign = Expression.Assign(field, newValue);
+
+            return Expression.Lambda<Action<T, T2>>(assign, param, newValue).CompileFast();
+        }
+
+        #endregion Setter
+
+        #endregion UnknownTypes
     }
 }
